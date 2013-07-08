0abfbf7270cb943c65cf1c1b7268ec827b7c511d
Christopher Lee <chris.lee@canonical.com>
2013-07-08 15:07:42 +1200

    Using gi.repository for Notify, modify a test as example

--- tests/autopilot/unity8/tests/testnotifications.py
+++ tests/autopilot/unity8/tests/testnotifications.py
@@ -33,6 +33,7 @@ from autopilot.matchers import Eventually
 from autopilot.display import Display
 from autopilot.platform import model
 
+from gi.repository import GLib, Notify
 import unittest
 import time
 import os
@@ -50,26 +51,69 @@ class NotificationTestCase(ShellTestCase, TestShellHelpers):
         self.touch = Touch.create()
         super(NotificationTestCase, self).setUp("768x1280", "18")
 
+        Notify.init("Autopilot Notification Tests")
+        self.addCleanup(Notify.uninit)
+
+    def create_notification(self, title="", body="", asset=None, urgency='NORMAL'):
+        logger.info("Creating notification with title(%s) body(%s) and urgency(%r)", title, body, urgency)
+        n = Notify.Notification.new(title, body, asset)
+        n.set_urgency(self._get_urgency(urgency))
+        n.set_timeout(10000)
+
+        return n
+
+    def _get_urgency(self, urgency):
+        """Translates urgency string to enum."""
+        _urgency_enums = {
+            'LOW': Notify.Urgency.LOW,
+            'NORMAL': Notify.Urgency.NORMAL,
+            'CRITICAL': Notify.Urgency.CRITICAL,
+        }
+
+        return _urgency_enums.get(urgency.upper())
+
+    def get_icon_path(self, icon_name):
+        """Given an icons file name returns the full path (either system or
+        source tree.
+
+        Consider the graphics directory as root so for example (runnign tests
+        from installed unity8-autopilot package):
+        >>> self.get_icon_path('clock@18.png')
+        /usr/share/unity8/graphics/clock@18.png
+
+        >>> self.get_icon_path('applicationIcons/facebook@18.png')
+        /usr/share/unity8/graphics/applicationIcons/facebook@18.png
+
+        """
+        if os.path.abspath(__file__).startswith('/usr/'):
+            return '/usr/share/unity8/graphics/' + icon_name
+        else:
+            return os.path.abspath(os.getcwd() + "/../../graphics/" + icon_name)
+
     def get_notifications_list(self):
         main_view = self.main_window.get_qml_view()
         return main_view.select_single("QQuickListView", objectName='notificationList')
 
 class TestNotifications(NotificationTestCase):
     def test_icon_summary_body(self):
+        """Notification must display the expected summary and body text."""
         notify_list = self.get_notifications_list()
-        subprocess.call(["notify-send",
-                         "Icon-Summary-Body",
-                         "Hey pal, what's up with the party next weekend? Will you join me and Anna?",
-                         "--icon=" + os.getcwd() + "/../../graphics/avatars/anna_olsson@12.png",
-                         "--urgency=normal",
-                         "--hint=string:x-canonical-secondary-icon:" + os.getcwd() + "/../../graphics/applicationIcons/phone-app@18.png"])
         self.unlock_greeter()
+
+        summary = "Icon-Summary-Body"
+        body = "Hey pal, what's up with the party next weekend? Will you join me and Anna?"
+        icon_path = self.get_icon_path('avatars/anna_olsson@12.png')
+        hint_icon = self.get_icon_path('applicationIcons/phone-app@18.png')
+
+        notification = self.create_notification(summary, body, icon_path)
+        notification.set_hint('x-canonical-secondary-icon', GLib.Variant.new_string(hint_icon))
+        notification.show()
 
         get_notification = lambda: notify_list.select_single('Notification')
         self.assertThat(get_notification, Eventually(NotEquals(None)))
         notification = get_notification()
-        self.assertThat(notification.summary, Equals("Icon-Summary-Body"))
-        self.assertThat(notification.body, Equals("Hey pal, what's up with the party next weekend? Will you join me and Anna?"))
+        self.assertThat(notification.summary, Equals(summary))
+        self.assertThat(notification.body, Equals(body))
 
     def test_icon_summary(self):
         notify_list = self.get_notifications_list()
