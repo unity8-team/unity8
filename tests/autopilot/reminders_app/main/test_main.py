0ec51ff0c1ac1c6328d8bfd5b21d9c2ff33cc0c5
Michael Zanetti <michael.zanetti@canonical.com>
2013-11-22 19:13:40 +0100

    "fix" autopilot tests

--- tests/autopilot/reminders_app/main/test_main.py
+++ tests/autopilot/reminders_app/main/test_main.py
@@ -7,13 +7,13 @@ from textwrap import dedent
 from testtools.matchers import Is, Not, Equals
 from testtools import skip
 import os
-from reminders-app import UbuntuTouchAppTestCase
+from reminders_app import UbuntuTouchAppTestCase
 
 
 class GenericTests(UbuntuTouchAppTestCase):
     """Generic tests for the Hello World"""
 
-    test_qml_file = "%s/%s.qml" % (os.path.dirname(os.path.realpath(__file__)),"../../../../reminders-app")
+    test_qml_file = "%s/%s.qml" % (os.path.dirname(os.path.realpath(__file__)),"../../../../src/app/qml/reminders-app")
 
     def test_0_can_select_mainView(self):
         """Must be able to select the mainview."""
@@ -22,17 +22,4 @@ class GenericTests(UbuntuTouchAppTestCase):
         self.assertThat(mainView.visible,Eventually(Equals(True)))
 
 
-    def test_1_init_label(self):
-        """Check the initial text of the label"""
-
-        lbl = self.get_object(objectName="label")
-        self.assertThat(lbl.text, Equals("Hello.."))
-
-
-    def test_can_tap_button(self):
-        """Must be able to tap the button"""
-
-        lbl = self.get_object(objectName="label")
-        self.mouse_click(objectName="button")
-        self.assertThat(lbl.text, Eventually(Equals("..world!")))
 
