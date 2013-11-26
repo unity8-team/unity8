ce4a460b6725e17aa5d253e38d9e589f2a02e763
Michael Zanetti <michael.zanetti@canonical.com>
2013-11-26 18:18:33 +0100

    fix copyright headers in all our code files. added COPYING file.

--- src/plugin/Evernote/jobs/fetchnotesjob.cpp
+++ src/plugin/Evernote/jobs/fetchnotesjob.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright: 2013 Canonical, Ltd
+ *
+ * This file is part of reminders-app
+ *
+ * reminders-app is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * reminders-app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Michael Zanetti <michael.zanetti@canonical.com>
+ */
+
 #include "fetchnotesjob.h"
 
 #include "notesstore.h"
