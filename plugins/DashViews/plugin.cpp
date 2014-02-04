069668d6d552fdb88e491a68ebe4bdfe0ced6bed
Ubuntu daily release <ps-jenkins@lists.canonical.com>
2014-02-04 14:09:14 +0000

    Import upstream version 7.84+14.04.20140204

--- plugins/DashViews/plugin.cpp
+++ plugins/DashViews/plugin.cpp
@@ -17,7 +17,9 @@
 
 #include "plugin.h"
 
+#include "horizontaljournal.h"
 #include "listviewwithpageheader.h"
+#include "organicgrid.h"
 #include "verticaljournal.h"
 
 #include <QAbstractItemModel>
@@ -26,6 +28,8 @@ void DashViewsPlugin::registerTypes(const char *uri)
 {
     Q_ASSERT(uri == QLatin1String("DashViews"));
     qmlRegisterType<QAbstractItemModel>();
+    qmlRegisterType<HorizontalJournal>(uri, 0, 1, "HorizontalJournal");
     qmlRegisterType<ListViewWithPageHeader>(uri, 0, 1, "ListViewWithPageHeader");
+    qmlRegisterType<OrganicGrid>(uri, 0, 1, "OrganicGrid");
     qmlRegisterType<VerticalJournal>(uri, 0, 1, "VerticalJournal");
 }
