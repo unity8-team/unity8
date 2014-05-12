4cc4d67da91aa81a8fa09b03d11ce1db195c57be
David Planella <david.planella@ubuntu.com>
2014-05-12 12:53:46 +0200

    Added OpenSSL license exception

--- src/plugin/Evernote/evernoteplugin.h
+++ src/plugin/Evernote/evernoteplugin.h
@@ -1,25 +1,25 @@
-/*****************************************************************************
- * Copyright: 2013 Michael Zanetti <michael_zanetti@gmx.net>                 *
- *                                                                           *
- * This file is part of ubuntu-fitbit-app                                    *
- *                                                                           *
- * ubuntu-fitbit-app is free software: you can redistribute it and/or modify *
- * it under the terms of the GNU General Public License as published by      *
- * the Free Software Foundation, either version 3 of the License, or         *
- * (at your option) any later version.                                       *
- *                                                                           *
- * ubuntu-fitbit-app is distributed in the hope that it will be useful,      *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
- * GNU General Public License for more details.                              *
- *                                                                           *
- * You should have received a copy of the GNU General Public License         *
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
- *                                                                           *
- ****************************************************************************/
+/*
+ * Copyright: 2013 Canonical, Ltd
+ *
+ * This file is part of reminders
+ *
+ * reminders is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * reminders is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Michael Zanetti <michael.zanetti@canonical.com>
+ */
 
-#ifndef FITBITPLUGIN_H
-#define FITBITPLUGIN_H
+#ifndef EVERNOTEPLUGIN_H
+#define EVERNOTEPLUGIN_H
 
 #include <QQmlExtensionPlugin>
 
@@ -27,9 +27,9 @@ class EvernotePlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
-public:    
+public:
     void registerTypes(const char *uri);
     void initializeEngine(QQmlEngine *engine, const char *uri);
 };
 
-#endif // FITBITPLUGIN_H
+#endif // EVERNOTEPLUGIN_H
