64df05dcba83b29f1d7c2d8f2922979a81fea710
Leo Arias <leo.arias@canonical.com>
2014-06-25 15:42:30 -0600

    Use the fake home fixture from the toolkit.

--- tests/autopilot/reminders/tests/__init__.py
+++ tests/autopilot/reminders/tests/__init__.py
@@ -53,43 +53,20 @@ class BaseTestCaseWithTempHome(AutopilotTestCase):
         os.system('pkill signond')
 
     def _patch_home(self):
-        """ mock /home for testing purposes to preserve user data
-        """
-        temp_dir_fixture = fixtures.TempDir()
-        self.useFixture(temp_dir_fixture)
-        temp_dir = temp_dir_fixture.path
-        temp_xdg_config_home = os.path.join(temp_dir, '.config')
-
-        # If running under xvfb, as jenkins does,
-        # xsession will fail to start without xauthority file
-        # Thus if the Xauthority file is in the home directory
-        # make sure we copy it to our temp home directory
-        self._copy_xauthority_file(temp_dir)
-
+        fake_home_fixture = toolkit_fixtures.FakeHome()
+        self.useFixture(fake_home_fixture)
+        temp_xdg_config_home = os.path.join(
+            fake_home_fixture.directory, '.config')
         # click requires using initctl env (upstart), but the desktop can set
         # an environment variable instead
         self.useFixture(
             toolkit_fixtures.InitctlEnvironmentVariable(
-                HOME=temp_dir, XDG_CONFIG_HOME=temp_xdg_config_home))
-        self.useFixture(
-            fixtures.EnvironmentVariable('HOME', newvalue=temp_dir))
+            XDG_CONFIG_HOME=temp_xdg_config_home))
         self.useFixture(
             fixtures.EnvironmentVariable(
                 'XDG_CONFIG_HOME',  newvalue=temp_xdg_config_home))
 
-        logger.debug('Patched home to fake home directory ' + temp_dir)
-
-        return temp_dir
-
-    def _copy_xauthority_file(self, directory):
-        """ Copy .Xauthority file to directory, if it exists in /home
-        """
-        xauth = os.path.expanduser(os.path.join('~', '.Xauthority'))
-        if os.path.isfile(xauth):
-            logger.debug("Copying .Xauthority to " + directory)
-            shutil.copyfile(
-                os.path.expanduser(os.path.join('~', '.Xauthority')),
-                os.path.join(directory, '.Xauthority'))
+        return fake_home_fixture.directory
 
 
 class RemindersAppTestCase(BaseTestCaseWithTempHome):
