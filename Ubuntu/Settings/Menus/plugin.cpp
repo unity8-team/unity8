7ddd5c8dd217608ddc1a02fead4fc2e973bcc0e5
Nick Dedekind <nicholas.dedekind@gmail.com>
2014-06-27 10:35:26 +0100

    review comments

--- Ubuntu/Settings/Menus/plugin.cpp
+++ Ubuntu/Settings/Menus/plugin.cpp
@@ -1,26 +1,19 @@
 /*
- * Copyright 2013 Canonical Ltd.
+ * Copyright (C) 2014 Canonical, Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Authors:
- *      Nick Dedekind <nick.dedekind@canonical.com>
  */
 
-// Qt
-#include <QtQml/qqml.h>
-
-// self
 #include "plugin.h"
 
 void UbuntuSettingsComponentsPlugin::registerTypes(const char *uri)
