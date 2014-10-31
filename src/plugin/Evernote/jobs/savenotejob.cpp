bb58b7e9737acade42256010e0d7629266ce1609
Riccardo Padovani <rpadovani@ubuntu.com>
2014-10-31 11:54:57 +0100

    Fixed typo

--- src/plugin/Evernote/jobs/savenotejob.cpp
+++ src/plugin/Evernote/jobs/savenotejob.cpp
@@ -62,7 +62,7 @@ void SaveNoteJob::startJob()
     note.__isset.notebookGuid = true;
 
     note.updated = m_note->updated().toMSecsSinceEpoch();
-    note.__isset.updated= true;
+    note.__isset.updated = true;
 
     std::vector<evernote::edam::Guid> tags;
     foreach (const QString &tag, m_note->tagGuids()) {
