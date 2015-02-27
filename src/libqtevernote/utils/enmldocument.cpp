57cec08b2e0148e03f05ede450d927e50e8f68e7
Michael Zanetti <michael.zanetti@canonical.com>
2015-02-28 00:20:20 +0100

    fix image filenames (jpg vs jpeg)

--- src/libqtevernote/utils/enmldocument.cpp
+++ src/libqtevernote/utils/enmldocument.cpp
@@ -159,7 +159,8 @@ QString EnmlDocument::convert(const QString &noteGuid, EnmlDocument::Type type) 
                     if (type == TypeRichText) {
                         writer.writeAttribute("src", composeMediaTypeUrl(mediaType, noteGuid, hash));
                     } else if (type  == TypeHtml) {
-                        QString imagePath = NotesStore::instance()->storageLocation() + hash + "." + mediaType.split('/').last();
+                        QString fileName = NotesStore::instance()->note(noteGuid)->resource(hash)->fileName();
+                        QString imagePath = NotesStore::instance()->storageLocation() + hash + "." + fileName.split('.').last();
                         writer.writeAttribute("src", imagePath);
                         writer.writeAttribute("id", "en-attachment/" + hash + "/" + mediaType);
                     }
