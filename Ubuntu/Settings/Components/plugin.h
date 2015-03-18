d18d4aa3025e88fbf684fea7184dbc068442e3c2
Nick Dedekind <nick.dedekind@canonical.com>
2015-03-18 10:12:39 +0000

    typo

--- Ubuntu/Settings/Components/plugin.h
+++ Ubuntu/Settings/Components/plugin.h
@@ -14,8 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef UBUNTUSETTINGSCOMPOENNTS_PLUGIN_H
-#define UBUNTUSETTINGSCOMPOENNTS_PLUGIN_H
+#ifndef UBUNTUSETTINGSCOMPONENTS_PLUGIN_H
+#define UBUNTUSETTINGSCOMPONENTS_PLUGIN_H
 
 #include <QtQml/QQmlExtensionPlugin>
 
@@ -27,4 +27,4 @@ public:
     void registerTypes(const char *uri);
 };
 
-#endif // UBUNTUSETTINGSCOMPOENNTS_PLUGIN_H
+#endif // UBUNTUSETTINGSCOMPONENTS_PLUGIN_H
