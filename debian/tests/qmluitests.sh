c83b7320ef29686134c818838a9cb99e4f517c03
Daniel d'Andrada <daniel.dandrada@canonical.com>
2017-02-23 11:24:48 -0300

    WIP - Attempt to get tests to run in jenkins CI

--- debian/tests/qmluitests.sh
+++ debian/tests/qmluitests.sh
@@ -8,4 +8,6 @@ DEB_HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 
 export ARTIFACTS_DIR="${ADT_ARTIFACTS}"
 
+export MIR_SERVER_PLATFORM_GRAPHICS_LIB="/usr/lib/${DEB_HOST_MULTIARCH}/mir/server-platform/server-mesa-x11.so.12"
+
 /usr/lib/$DEB_HOST_MULTIARCH/unity8/tests/scripts/xvfballtests.sh
